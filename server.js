97f29e68ae41c1d3dd420219da4f27e2a61845ca
unknown <mylene.obejero@g.msuiit.edu.ph>
2026-01-30 15:22:14 +0800

    Troubleshooting node.js issues

--- server.js
+++ server.js
@@ -1,42 +1,54 @@
-const express = require("express");
+ const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
-// Temporary in-memory storage
-let jobs = [];
+// MongoDB connection
+mongoose
+  .connect(
+   "mongodb+srv://jobtracker:jobtracker123@cluster0.zb0nub9.mongodb.net/?appName=Cluster0"
+  )
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch((err) => console.error("❌ MongoDB error:", err.message));
 
-// Home route
-app.get("/", (req, res) => {
-  res.send("Job Tracker API is running 🚀");
+// Job Schema
+const jobSchema = new mongoose.Schema({
+  company: { type: String, required: true },
+  role: { type: String, required: true },
+  status: { type: String, required: true },
 });
 
-// GET all jobs
-app.get("/jobs", (req, res) => {
+const Job = mongoose.model("Job", jobSchema);
+
+// Home
+app.get("/", (req, res) => {
+  res.send("Job Tracker API with MongoDB 🚀");
+});
+
+// GET jobs
+app.get("/jobs", async (req, res) => {
+  const jobs = await Job.find();
   res.json(jobs);
 });
 
-// POST a new job
-app.post("/jobs", (req, res) => {
+// POST job
+app.post("/jobs", async (req, res) => {
   const { company, role, status } = req.body;
 
   if (!company || !role || !status) {
-    return res.status(400).json({ message: "All fields are required" });
+    return res.status(400).json({ message: "All fields required" });
   }
 
-  const newJob = {
-    id: Date.now(),
-    company,
-    role,
-    status,
-  };
+  const newJob = new Job({ company, role, status });
+  await newJob.save();
 
-  jobs.push(newJob);
   res.status(201).json(newJob);
 });
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
